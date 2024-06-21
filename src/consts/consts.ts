d631d3883fee66b1ee7e435eb1720c60484f339d
Edinson Rivera <edirju@gmail.com>
2024-06-21 13:15:30 -0500

    update hero + SkillSlider

---
+++ src/consts/consts.ts
@@ -0,0 +1,103 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = "Edinson Rivera Jurado";
+export const SITE_DESCRIPTION = "Ingeniero de Sistemas con intereses en desarrollo web y marketing digital con +5 años de experiencia & creador de contenido";
+
+export const BASIC_INFO = {
+  name: "Edinson Rivera Jurado",
+  label: "Ingeniero de Sistemas",
+  image: "/images/edison.jpg",
+  social: {
+    github: "https://github.com/edisonrivera",
+    linkedin: "https://www.linkedin.com/in/edisonrivera/",
+    twitter: "https://twitter.com/edisonrivera",
+    instagram: "https://www.instagram.com/edisonrivera/",
+    youtube: "https://www.youtube.com/channel/UC0b-y-Z8X-Y-V6yK-v9v-Ig",
+    facebook: "https://www.facebook.com/edisonrivera",
+    medium: "https://medium.com/@edisonrivera",
+    codepen: "https://codepen.io/edisonrivera",
+    devto: "https://dev.to/edisonrivera",
+    stackoverflow: "https://stackoverflow.com/users/100010/edisonrivera",
+    linkedin_group: "https://www.linkedin.com/groups/edisonrivera-2832a31a-ae0d-4ee1-bd0b-d0a0ef0c9e0a/",
+  },
+};
+
+export const LANDING_HERO = {
+  title: "Edinson Rivera",
+  description: "Ingeniero de Sistemas",
+  image: "/images/projects/edison.jpg",
+  greetings: [
+    'hola',
+    'hello',
+    'hi',
+    'hey',
+  ]
+};
+
+export const PROJECTS = [
+  {
+    title: "Edison Rivera",
+    description: "Ingeniero de Sistemas",
+    image: "/images/projects/edison.jpg",
+    link: "https://edisonrivera.com",
+  },
+  {
+    title: "Edison Rivera",
+    description: "Ingeniero de Sistemas",
+    image: "/images/projects/edison.jpg",
+    link: "https://edisonrivera.com",
+  },
+];  
+
+export const SKILLS = [
+  {
+    title: "HTML",
+    description: "Frontend",
+  },
+  {
+    title: "CSS",
+    description: "Frontend",
+  },
+  {
+    title: "JavaScript",
+    description: "Frontend",
+  },
+];    
+
+export const BLOG_POSTS = [
+  {
+    title: "Edison Rivera",
+    description: "Ingeniero de Sistemas",
+    image: "/images/projects/edison.jpg",
+    link: "https://edisonrivera.com",
+  },
+  {
+    title: "Edison Rivera",
+    description: "Ingeniero de Sistemas",
+    image: "/images/projects/edison.jpg",
+    link: "https://edisonrivera.com",
+  },
+];  
+
+export const CONTACT = {
+  name: "Edison Rivera",
+  email: "edisonrivera@gmail.com",
+  phone: "+55 (555) 555-5555",
+  address: "Edison Rivera, Edison Rivera, Edison Rivera, Edison Rivera",
+};
+
+export const SOCIAL_LINKS = [
+  {
+    name: "GitHub",
+    link: "https://github.com/edisonrivera",
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/edisonrivera/",
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/edisonrivera",
+  },    
+];
+
